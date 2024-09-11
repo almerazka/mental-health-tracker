d933e52c9ad328d089e6c0dcf6b7684e07869051
almerazka <almer.azka12@gmail.com>
2024-09-11 14:39:36 +0700

    test

--- main/models.py
+++ main/models.py
@@ -1,8 +1,10 @@
+import uuid  # tambahkan baris ini di paling atas
 from django.db import models
 
 # kelas dasar
 class MoodEntry(models.Model): # nama model
     # atribut atau field pada model
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
 # Berkas urls.py pada aplikasi mengatur rute URL spesifik untuk fitur-fitur dalam aplikasi tersebut.
 app_name = 'main'
 
 urlpatterns = [ #mengatur rute URL yang terkait dengan aplikasi main
     path('', show_main, name='show_main'),
+    path('create-mood-entry', create_mood_entry, name='create_mood_entry'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
 ]
